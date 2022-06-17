9c97ff29cba4922ce6fd539b66cf04733ee8dc96
SheilaaaGomes <88672784+SheilaaaGomes@users.noreply.github.com>
2022-06-17 16:57:23 -0300

    Completei o script incluirpet (Post)

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -12,6 +12,7 @@ import java.nio.file.Paths;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.contains;
 
 // 3 - Classe
 public class Pet {
@@ -43,6 +44,8 @@ public class Pet {
                 .statusCode(200)
                 .body("name", is("Sheila"))
                .body("status", is("available"))
+               //.body("category.nome", contains("dog"))
+               .body("category.nome", is("dog"))
 
        ;
 
