97e36b5321f769912f75fcc85ea2f82a8855559e
SheilaaaGomes <88672784+SheilaaaGomes@users.noreply.github.com>
2022-06-20 13:51:50 -0300

    Completei o script consultarPet

--- src/test/java/petstore/pet.java
+++ src/test/java/petstore/pet.java
@@ -1,42 +1,35 @@
 // 1 - Pacote
 package petstore;
-
 // 2 - Bibliotecas
-
-import io.restassured.internal.RestAssuredResponseOptionsGroovyImpl;
-import javafx.animation.PathTransitionBuilder;
 import org.testng.annotations.Test;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.contains;
-import static org.omg.IOP.CodecPackage.TypeMismatchHelper.extract;
-
 // 3 - Classe
 public class pet {
     // 3.1 - Atributos
     String uri = "https://petstore.swagger.io/v2/pet"; // endereço da entidade Pet
 
     // 3.2 - Métodos e Funções
+    // 3.2 - Métodos e Funções
     public String lerJson(String caminhoJson) throws IOException {
         return new String(Files.readAllBytes(Paths.get(caminhoJson)));
     }
 
     // Incluir - Create - Post
-    @Test(priority = 1)
     // Identifica o método ou função como um teste para o TestNG
+    @Test(priority = 1)  // Identifica o método ou função como um teste para o TestNG
     public void incluirPet() throws IOException {
         String jsonBody = lerJson("db/pet1.json");
 
         // Sintaxe Gherkin
         // Dado - Quando - Então
-        // Given - When - Then
+       // Given - When - Then
 
-       given() // Script Rest
+        given() // Dado
                 .contentType("application/json") // comum em API REST - antigas era "text/xml"
                 .log().all()
                 .body(jsonBody)
@@ -46,37 +39,38 @@ public class pet {
                 .log().all()
                 .statusCode(200)
                 .body("name", is("Sheila"))
-               .body("status", is("available"))
-               //.body("category.nome", contains("dog"))
-               .body("category.nome", is("dog"));
+                .body("status", is("available"))
+                .body("category.name", is("dog"))
+                .body("tags.name", contains("sta"))
+                .body("category.name", is("dog"))
+                .body("tags.name", contains("sta"))
+        ;
+
 
     }
-    //Método Consulta
-    @Test(priority = 2)
-    public void consultarpet(){
+
+    @Test(priority=2)
+    public void consultarPet(){
         String petId = "77910321";
 
-        given()
-                    .contentType("aplication/json")
-                    .log() .all()
-                .when()
-                    .get(uri + "/" + petId)
-                .then()
-                    .log() .all()
-                    .statusCode(200)
-                .body("name", is("Sheila"))
-                .body("category.name", is("dog"))
-                .body("status", is("available"));
-        
-            .extract()
-                .path("category.name");
+        String token =
+                given()
+                        .contentType("application/json")
+                        .log().all()
+                        .when()
+                        .get(uri + "/" + petId)
+                        .then()
+                        .log().all()
+                        .statusCode(200)
+                        .body("name", is("Sheila"))
+                        .body("category.name", is("dog"))
+                        .body("status",is("available"))
+                        .extract()
+                        .path("category.name")
+                ;
+        System.out.println("O token é " + token);
 
-        String token;
-        System.out.println("O token é" + token);
-
-    }
 
 
     }
-
 }
